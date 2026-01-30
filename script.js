9c57e8688a970eca00c71662bc80539007287d8a
ayushmdalal-lang <ayushmdalal@gmail.com>
2026-01-30 22:58:48 +0530

    Refactor and enhance puzzle game functionality

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ if (toggle) {
 }
 
 // ---------- SLIDES ----------
-const slides = ["slides/1.jpg", "slides/2.png", "slides/3.png"];
+const slides = ["slides/1.jpg", "slides/2.jpg", "slides/3.jpg"];
 const captions = [
   "A little moment I like 🌸",
   "This one always makes me smile ✨",
@@ -281,4 +281,5 @@ function enableTouchDrag(piece) {
     piece.touchId = null;
   });
 }
+
 
